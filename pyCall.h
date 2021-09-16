8d2c17a115bc033c108f86de33578937545cc6ce
unknown <kjsheng@iflytek.com>
2021-09-16 16:52:20 +0800

    add log h

--- pyCall.h
+++ pyCall.h
@@ -1,7 +1,13 @@
 #ifndef PY_CALL_H
 #define PY_CALL_H
 #include "include/python/Python.h"
+// log
+#include "include/spdlog/include/spdlog/spdlog.h"
+#include "include/spdlog/include/spdlog/sinks/rotating_file_sink.h"
 
+// utils
+#include "include/utils/utils.h"
+#include "include/utils/json.hpp"
 int callWrapperInit();
 int callWrapperFini();
 
