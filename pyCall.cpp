bf733e4a582ce66e920bec61e185c93cc038dbea
unknown <kjsheng@iflytek.com>
2021-09-25 20:14:16 +0800

    fix

--- pyCall.cpp
+++ pyCall.cpp
@@ -236,7 +236,7 @@ int callWrapperExec(const char *usrTag, pParamList params, pDataList reqData, pD
                         return ret;
                     }
 
-                    ret = pyDictStrToChar(tmpDict, DATA_DATA, tmpData->data, sid);
+                    ret = pyDictStrToChar(tmpDict, DATA_DATA, (char*)tmpData->data, sid);
                     if (ret != 0)
                     {
                         return ret;
