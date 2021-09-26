0a56e4fbd20a77b5028b919f8b78042ae2fbdff0
unknown <kjsheng@iflytek.com>
2021-09-26 10:37:52 +0800

    fix

--- pyCall.cpp
+++ pyCall.cpp
@@ -238,7 +238,7 @@ int callWrapperExec(const char *usrTag, pParamList params, pDataList reqData, pD
                     {
                         return ret;
                     }else{
-                        std::cout<<"转换后的字符串"<<tmpRltKey<<std::endl;
+                        std::cout<<"转换后的字符串"<<tmpRltKey<<"长度"<<strlen(tmpRltKey.c_str())<<std::endl;
                         tmpData->key = (char *)malloc(strlen(tmpRltKey.c_str()));
 	                    memcpy(tmpData->key,(char*)tmpRltKey.c_str(), strlen(tmpRltKey.c_str()));
                         std::cout<<tmpData->key<<std::endl;
