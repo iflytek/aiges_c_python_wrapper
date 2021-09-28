0e34a1f1f890b20af4a5843af32986475d67ee77
unknown <kjsheng@iflytek.com>
2021-09-28 20:53:54 +0800

    fix

--- pyCall.cpp
+++ pyCall.cpp
@@ -165,8 +165,8 @@ int callWrapperExec(const char *usrTag, pParamList params, pDataList reqData, pD
                 PyObject *pyKey = PyUnicode_FromString(p->key);
                 PyDict_SetItemString(tmp, "key", pyKey);
 
-                std::string datas(static_cast<char*>(p->data),p->len);
-                PyObject *pyData = PyUnicode_FromString(datas.c_str());
+                //std::string datas(static_cast<char*>(p->data),p->len);
+                PyObject *pyData = Py_BuildValue("O", p->data);
                 PyDict_SetItemString(tmp, "data", pyData);
 
                 PyObject *pyDataLen = Py_BuildValue("i", int(p->len));
